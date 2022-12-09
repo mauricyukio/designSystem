7afea430133b01424adb32fbbb8c639552931cda
mauricyukio <myynagashima@gmail.com>
2022-12-09 10:59:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 const documentationLink = document.querySelector('#documentation-link');
-
-documentationLink.addEventListener('click', function toggleLightMode() {
-  setTimeout(() => {
-    document.querySelector('body').classList.add('light-mode');
-  }, 2000);
-});
